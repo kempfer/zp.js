5f1c031bf33247156fb09dca524bd3be345c67ee
Maxim <zotov_mv@groupbwt.com>
2015-04-27 21:39:46 +0300

    Add new comment core/zp

--- core/zp.js
+++ core/zp.js
@@ -7,7 +7,8 @@
 	var zp = {
 		
 		/**
-		*
+		* Возращает уникалное значение для текущей сесии
+		*@return Number
 		*/
 		uniqueId : (function() {
 			var id = 0;
@@ -17,59 +18,75 @@
 		})(),
 		
 		/**
-		*
-		*
+		* Проверяет является ли item Function
+		*@param mixed item
+		*@return Boolean 
 		**/
 		isFunction : function (item) {
 			return typeof item === "function" ;
 		},
 		
 		/**
-		*
-		*
+		* Проверяет является ли item Object
+		*@param mixed item
+		*@return Boolean 
 		**/
 		isObject : function (item) {
 			return Object.prototype.toString.call(item) === '[object Object]';
 		},
 		
 		/**
-		*
-		*
+		* Проверяет является ли item String
+		*@param mixed item
+		*@return Boolean 
 		**/
 		isString : function (item) {
 			return typeof item === "string";
 		},
 		
 		/**
-		*
-		*
+		* Проверяет является ли item Boolean
+		*@param mixed item
+		*@return Boolean 
 		**/
 		isBoolean : function (item) {
 			return typeof item === "boolean";
 		},
+		
 		/**
-		*
-		*
+		* Проверяет является ли item Number
+		*@param mixed item
+		*@return Boolean 
 		**/
 		isNumber : function (item) {
 			return typeof item === "number" && isFinite(item);
 		},
 		
 		/**
-		*
-		*
+		* Проверяет является ли item Array
+		*@param mixed item
+		*@return Boolean 
+		**/
+		isArray : function (item) {
+			return Array.isArray(item);
+		},
+		
+		/**
+		* Возращает пустую функцию 
+		*@return Function 
 		**/
 		emptyFunc : function () {},
 		
 		/**
 		*
-		*
+		*@return Object 
 		**/
 		globalScope : (typeof window === 'undefined') ? this : window,
 		
 		/**
-		*
-		*
+		* Розшерям нашу library 
+		*@param string name
+		*@param Object|Function item
 		**/
 		expand : function (name, item) {
 			if(zp.isEmpty(zp[name])){
@@ -78,18 +95,19 @@
 		},
 		
 		/**
-		*
-		*
+		* Проверяет на пустоту переданное значение
+		*@param mixed item
+		*@return Boolean 
 		**/
 		isEmpty : function (item) {
 			return 	(item === false ) 									||
 					(item === 0) 										||
 					(item === null)										||
 					(item === undefined )								||
-					(Teq.isArray(item) && item.length === 0) 			||
+					(zp.isArray(item) && item.length === 0) 			||
 					(item === "" )										||
-					(t.isObject(item) && Object.keys(item).length === 0);
-		},
+					(zp.isObject(item) && Object.keys(item).length === 0);
+		}
 	};
 	
 	zp.globalScope['zp'] = zp;
