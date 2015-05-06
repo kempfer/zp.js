aa5ed62b87596db2eb8f95dcffb7b6d166920018
Maxim <zotov_mv@groupbwt.com>
2015-05-06 21:20:51 +0300

    move folder core and plugins to src
    Signed-off-by: Maxim <zotov_mv@groupbwt.com>

---
+++ src/plugins/ajax.js
@@ -0,0 +1,182 @@
+/**
+ * Created by zotov_mv on 30.04.2015.
+ */
+;(function (zp) {
+
+    "use strict";
+
+    var
+        /**
+         *
+         * @param object|string|number
+         * @returns {string}
+         */
+        stringify = function ajaxStringify (object) {
+            var 
+                i,
+                array = [], 
+                e = encodeURIComponent;
+            if (zp.isEmpty(object)){
+                return '';  
+            } 
+            if(!zp.isUndefined(window.FormData) && object instanceof window.FormData){
+                return object;
+            }
+            if (zp.isString(object) || zp.isNumber(object)) {
+                return String( object );  
+            } 
+            for (i in object){
+                if (object.hasOwnProperty(i)) {
+                    array.push( e(i) + '=' + e(object[i]) );
+                }
+            }
+            return array.join('&');
+        },
+        /**
+         *
+         * @param String url
+         * @param String method
+         * @param Object data
+         * @param Boolean cache
+         * @private
+         * @returns {string}
+         */
+        prepareUrl = function ajaxPrepareUrl (url,method,data,cache) {
+            var separator = url.indexOf( '?' ) == -1 ? '?' : '&';
+            if (method == 'GET' && data) {
+                url += separator + data;
+            }
+            if (!cache) {               
+                url += separator + 'rand=' + Date.now();
+            }   
+            return url;
+        },
+        /**
+         *
+         * @param string|object config
+         * @returns {Object}
+         */
+        prepareConfig = function ajaxPrepareConfig (config) {
+            var readyConfig;
+            if(zp.isString(config)){
+                readyConfig = ajax.defaultConfig;
+                readyConfig.url = config;
+            }
+            else{
+                readyConfig = zp.merger(ajax.defaultConfig,config);
+            }
+            return readyConfig;
+        },
+
+        /**
+         *
+         * @param XMLHttpRequest  xhr
+         * @param Object headers
+         * @param boolean sendFormData
+         */
+        setHeaders =  function ajaxSetHeaders (xhr,headers,isSendFormData) {
+            for(var key in headers){
+                if(isSendFormData && key == 'Content-Type'){
+                    continue;
+                }
+                xhr.setRequestHeader(key,headers[key]);
+            }
+            xhr.setRequestHeader('X-Requested-With','XMLHttpRequest');
+        },
+
+        /**
+         *
+         * @param XMLHttpRequest xhr
+         * @param  config
+         * @returns {Function}
+         */
+        onReady =  function ajaxOnReady (xhr, config) {      
+            return function ajaxOnReadyStateChange (e) {
+                if (xhr.readyState == 4) {              
+                    if (xhr.status == 200) {
+                        var result = xhr.responseText;
+                        if(config.responseType.toUpperCase() == 'JSON'){
+                            result = zp.decode(result);
+                        }
+                        if(zp.isFunction(config.onLoad)){
+                            config.onLoad(result,xhr,e);
+                        }
+                    }
+                    else{       
+                        var result = xhr.responseText;
+                        if(zp.isFunction(config.onError)){
+                            config.onError(result,xhr,e);
+                        }
+
+                    }               
+                }
+            }
+        },
+
+        /**
+         *
+         * @param Object config
+         * @returns {ajax}
+         */
+        ajax = function ajaxConstructor (config) {
+            if (! (this instanceof ajax)) {
+                return new ajax(config);
+            }
+            var 
+                readyConfig = prepareConfig(config),
+                method = readyConfig.method.toUpperCase(),
+                sendData = stringify(readyConfig.data),
+                url = prepareUrl(readyConfig.url,method,sendData,readyConfig.cache),
+                isSendFormData = !zp.isUndefined(window.FormData) && sendData instanceof window.FormData;
+            this.id = zp.uniqueId();
+            this.xhr = new XMLHttpRequest();
+            this.xhr.open(method, url, readyConfig.async);
+            this.xhr.timeout = readyConfig.timeout;
+            this.xhr.onreadystatechange = onReady(this.xhr, readyConfig);
+            this.xhr.onabort = zp.isFunction(readyConfig.onAbort) ? readyConfig.onAbort : null;
+            setHeaders(this.xhr, readyConfig.headers, isSendFormData);
+            this.xhr.send( method == 'POST' && sendData ? sendData : null );
+            return this;
+        };
+
+    ajax.prototype = {
+
+        constructor : ajax,
+
+        xhr : null,
+
+        id : null,
+
+        /**
+         *
+         * @returns {ajax}
+         */
+        abort : function ajaxAbort () {
+            this.xhr.abort();
+            return this;
+        }
+    };
+
+    /**
+     *
+     * @type {{method: string, type: string, timeout: number, data: {}, cache: boolean, url: string, headers: {X-Requested-With: string}, onAbort: zp.emptyFunc, onLoad: zp.emptyFunc, onError: zp.emptyFunc}}
+     */
+    ajax.defaultConfig = {
+        method : 'GET',
+        responseType : '',
+        timeout : 30000,
+        data : {},
+        cache : true,
+        async	: true,
+        url : location.href,
+        headers : {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        },
+        onAbort : zp.emptyFunc,
+        onLoad : zp.emptyFunc,
+        onError : zp.emptyFunc
+
+    };
+    zp.expand('ajax', ajax);
+
+})(zp);
---
+++ src/plugins/dom.js
@@ -0,0 +1,207 @@
+/**
+ * @author zotov_mv
+ * @see core/zp
+ */
+
+;(function (zp,document) {
+
+    "use strict";
+
+    var
+        ready = false,
+
+        onLoadList = [],
+
+        regexpSelector = {
+            findTag  	: /^[-_a-z0-9]+$/i,
+            findClass	: /^\.[-_a-z0-9]+$/i,
+            findId   	: /^#[-_a-z0-9]+$/i
+        },
+
+
+        ignoreCssPostfix =  {
+            zIndex: true,
+            fontWeight: true,
+            opacity: true,
+            zoom: true,
+            lineHeight: true
+        },
+        /**
+         *
+         * @param {string} str
+         * @returns {string}
+         */
+        camelCase = function domCamelCase (str) {
+            return String(str).replace(/-\D/g, function(match){
+                return match[1].toUpperCase();
+            });
+        },
+        /**
+         *
+         * @param {string} str
+         * @returns {string}
+         */
+        hyphenate = function domHyphenate (str) {
+            return String(str).replace(/[A-Z]/g, function(match){
+                return '-' + match[0].toLowerCase();
+            });
+        },
+
+        /**
+         *
+         */
+        readyFunc = function domReadyFunc () {
+            if(ready === true){
+                return;
+            }
+            ready = true;
+            for(var i = 0; i < onLoadList.length; i++){
+                if(zp.isFunction(onLoadList[i])){
+                    onLoadList[i]();
+                }
+            }
+            onLoadList = [];
+        },
+        /**
+         *
+         * @param {Object} node
+         * @param {Number} step
+         * @returns {dom}
+         */
+        findParentByStep = function domFindParentByStep (node, step) {
+            if (step == null || step < 0){
+                step = 1;
+            }
+            if (!node || step <= 0){
+                return new dom(node);
+            }
+            return findParentByStep(node.parentNode, step-1);
+        };
+
+
+    document.addEventListener('DOMContentLoaded', readyFunc, false);
+    window.addEventListener('load', readyFunc, false);
+
+
+    /**
+     *
+     * @constructor
+     * @this dom
+     * @param {String|Object} selector
+     * @param {Object} [context]
+     * @returns {dom}
+     */
+    var dom = function domConstructor (selector,context) {
+        if (! (this instanceof dom)) {
+            return new dom(selector,context);
+        }
+
+        if(!selector){
+            return this;
+        }
+
+        if (!arguments.length) {
+            this.nodes = [document];
+            return this;
+        }
+
+        var currentContext = context || document;
+
+        this.nodes =
+            zp.isString(selector)       ? dom.query(selector,currentContext) :
+            selector === window         ? [ document ] :
+            selector instanceof dom     ? zp.toArray(selector.nodes) :
+            dom.isElement(selector)     ? [selector] :
+            zp.isArray(selector)  		? selector :
+            [];
+        //Hak
+        this.nodes.forEach(function (el,index) {
+            this[index] = el;
+        }.bind(this));
+
+        this.length = this.nodes.length;
+
+        return this;
+    };
+
+    /**
+     *
+     * @property {Array.<Object>} nodes
+     * @property {Function} constructor
+     */
+    dom.prototype = {
+
+        constructor: dom,
+
+        length: 0,
+
+        splice: [].slice,
+
+        /**
+         * @pr
+         */
+        nodes: [],
+
+        /**
+         *
+         * @param {Number} index
+         * @returns {Object|null}
+         */
+        get: function (index) {
+            return document.body;
+            return this.nodes[Number(index) || 0];
+        },
+
+        /**
+         *
+         * @returns {Array.<*>}
+         */
+        toArray: function () {
+            return zp.toArray( this.nodes );
+        }
+    }
+    /**
+     *
+     * @param {object} node
+     * @returns {boolean}
+     */
+    dom.isElement = function domIsElement (node) {
+        return (node && node.nodeName) ? true : false;
+    };
+
+    /**
+     *
+     * @param {Function} callback
+     * @throws {TypeError} callback is function
+     */
+    dom.onLoad = function domOnReady (callback) {
+        if(!zp.isFunction(callback)){
+            throw new TypeError("callback is not a function");
+        }
+        if(ready){
+            setTimeout(callback,1);
+        }
+        else{
+            onReadyList.push(callback);
+        }
+    };
+
+    /**
+     *
+     * @param {string} selector
+     * @param {Object} [context]
+     * @returns {Object[]}
+     */
+    dom.query = function (selector,context) {
+        var currentContext = context ? context.getElementById ? context : document : document;
+
+        return 	selector.match(regexpSelector.findId)  		    ? [currentContext.getElementById(selector.substr(1))] :
+                selector.match(regexpSelector.findClass) 	    ? zp.toArray(currentContext.getElementsByClassName(selector.substr(1))) :
+                selector.match(regexpSelector.findTag)   	    ? zp.toArray(currentContext.getElementsByTagName  (selector)) :
+                zp.toArray(context.querySelectorAll(selector));
+    };
+
+
+    zp.expand('dom', dom);
+
+})(zp,document) ;
