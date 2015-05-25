5361553d3d103d928572973268e6eee8e6a2b54c
Maxim <zotov_mv@groupbwt.com>
2015-05-25 23:32:49 +0300

    fixed function define and change zpClass.define
    Signed-off-by: Maxim <zotov_mv@groupbwt.com>

--- src/plugins/class.js
+++ src/plugins/class.js
@@ -1,15 +1,12 @@
-/**
- * Created by zotov_000 on 14.05.2015.
- */
+
 /**
  * Created by Kempfer on 13.05.2015.
  */
-;(function (zp) {
+(function (zp) {
 
-     "use strict";
+    "use strict";
 
     var
-        F = function () {},
 
         slice = Array.prototype.slice,
 
@@ -20,17 +17,18 @@
          * @param {String} path
          * @param {Object} object
          */
-        define = function zpClassDefine (path, object){
+        define = function zpClassDefine (path, object) {
             var key,
                 part,
                 pathArray,
                 target = zp.globalScope;
             pathArray = path.split('.');
-            key  = pathArray.pop();
+            key = pathArray.pop();
             while (pathArray.length) {
                 part = pathArray.shift();
                 if (!target[part]) {
-                    target = target[part] = {};
+                    target[part] = {};
+                    target = target[part];
                 }
                 else {
                     target = target[part];
@@ -48,13 +46,13 @@
             var parent = this.Parent || resource[this.resource_id].parent,
                 fn = parent[methodName],
                 result;
-            if(zp.isFunction(fn)){
+            if (zp.isFunction(fn)) {
                 this.Parent = resource[parent.resource_id].parent;
                 if(arguments.length > 1) {
                     result = fn.apply(this, slice.call(arguments, 1));
                 }
                 else{
-                    result = fn.call(this)
+                    result = fn.call(this);
                 }
                 delete this.Parent;
             }
@@ -66,31 +64,31 @@
          * @param {Object} Class
          * @param {Object} object
          */
-        inheritParent = function zpClassInheritParent  (Class,object) {
+        inheritParent = function zpClassInheritParent  (Class, object) {
             var parent = object['extend'] || Object;
             Class.prototype = Object.create(parent.prototype);
             Class.prototype.resource_id = zp.uniqueId();
             resource[Class.prototype.resource_id] = {
-                parent : parent.prototype
-            }
+                parent: parent.prototype
+            };
             Class.prototype.callParent = callParent;
             delete object['extend'];
         },
 
         /**
          *
-         * @param {Function} Constructor
+         * @param {Function} constructor
          * @param {Object} object
          */
-        prepareClass = function zpClassPrepareClass (constructor,object) {
+        prepareClass = function zpClassPrepareClass (constructor, object) {
             var key;
             object = object || {};
-            inheritParent(constructor,object);
+            inheritParent(constructor, object);
             if (zp.isFunction(object)) {
-                object = { init: object };
+                object = {init: object};
             }
             for (key in object) {
-                if(object.hasOwnProperty(key)){
+                if (object.hasOwnProperty(key)) {
                     constructor.prototype[key] = object[key];
                 }
             }
@@ -101,13 +99,14 @@
 
             /**
              *
-             * @param {String} name
+             * @param {String} namespace
              * @param {Object} object
              */
-            define : function (name, object) {
+            define: function (namespace, object) {
+                object.name = namespace.split('.').pop();
                 var Constructor = zpClass.create(object);
-                Constructor.prototype.namespace = name;
-                define(name, Constructor);
+                Constructor.prototype.namespace = namespace;
+                define(namespace, Constructor);
             },
 
             /**
@@ -115,16 +114,16 @@
              * @param {Object} object
              * @returns {Object}
              */
-            create : function (object) {
+            create: function (object) {
                 var
-                    Constructor = function  zpClass () {
-                        if(Constructor.prototype.hasOwnProperty('init')){
-                            Constructor.prototype.init.apply(this,arguments);
+                    Constructor = function zpClass () {
+                        if (Constructor.prototype.init) {
+                            Constructor.prototype.init.apply(this, arguments);
                         }
                         return this;
                     },
                     name = object.hasOwnProperty('name') ? object['name'] : 'zpClass_' + zp.uniqueId();
-                prepareClass(Constructor,object);
+                prepareClass(Constructor, object);
                 Constructor.prototype.constructor = Constructor;
                 Constructor.prototype.name = name;
 
@@ -140,4 +139,4 @@
     zp.extend('define', zpClass.define);
     zp.extend('create', zpClass.create);
 
-})(zp);
+}(zp));
